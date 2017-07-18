7a032d94aef42a7169763803e7cfa25af10f49c3
Ellen Liu <ellen.m.liu@gmail.com>
2017-07-18 21:19:13 +0800

    Created database setup

---
+++ database_setup.py
@@ -0,0 +1,43 @@
+import sys
+
+from sqlalchemy import Column, ForeignKey, Integer, String
+
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+class Category(Base):
+    __tablename__ = 'category'
+    name = Column(String(80), nullable = False)
+    id = Column(Integer, primary_key = True)
+
+    @property
+    def serialize(self):
+        return{
+            'name': self.name,
+            'id': self.id
+            }
+
+class Item(Base):
+    __tablename__ = 'item'
+    name = Column(String(80), nullable = False)
+    id = Column(Integer, primary_key = True)
+    description = Column(String(250))
+    category_id = Column(Integer, ForeignKey('category.id'))
+    category = relationship(Category)
+
+    @property
+    def serialize(self):
+        return{
+            'name': self.name,
+            'description': self.description,
+            'id': self.id
+            }
+    
+####insert at end#####
+engine = create_engine(
+    'sqlite:///categoryitem.db')
+
+Base.metadata.create_all(engine)
